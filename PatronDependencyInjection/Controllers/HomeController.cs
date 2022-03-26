12b56c6f148f2d71eb4f2132c19c504812a5ca6c
Joaquín González <joaquin4525@hotmail.com>
2022-03-26 13:56:00 -0300

    Add project files.

---
+++ PatronDependencyInjection/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PatronDependencyInjection.Models;
+using System.Diagnostics;
+
+namespace PatronDependencyInjection.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Persona _persona;
+
+        public HomeController(Persona persona)
+        {
+            _persona = persona;
+        }
+
+        public IActionResult Index()
+        {
+            _persona.Nombre = "Joaquin";
+            _persona.Apellido = "Gonzalez";
+            _persona.Edad = 23;
+
+            return View(_persona);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ PatronDependencyInjection/Models/Persona.cs
@@ -0,0 +1,16 @@
+﻿namespace PatronDependencyInjection.Models
+{
+    public class Persona
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Edad { get; set; }
+
+        //public Persona(string nombre, string apellido, int edad)
+        //{
+        //    Nombre = nombre;
+        //    Apellido = apellido;
+        //    Edad = edad;
+        //}
+    }
+}
